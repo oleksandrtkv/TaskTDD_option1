fbba7f4c6fe49b574f36f4e22800acdfde59365f
oleksandrtkv <oleksandr.tkv@gmail.com>
2023-10-09 23:25:43 +0300

    create_testF

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,11 @@
 package main;
 
 public class Main {
+    private final double a = -0.5;
+    private final double b = 2.0;
+
+    public double f(double x) {
+        if (x <= 0.7) return 1;
+        return 0;
+    }
 }
--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -1,12 +1,26 @@
 package main;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class MainTest {
 
+    private final double EPS = 1e-6;
+
+    Main main;
+
     @BeforeMethod
     public void setUp() {
+        main = new Main();
+    }
+
+    @Test
+    public void testF() {
+        double expected = 1;
+        double x = 0.1;
+        double actual = main.f(x);
+        assertEquals(actual, expected, EPS);
     }
 }
