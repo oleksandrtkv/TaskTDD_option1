f74975837371886de659dfe1c8c9faccdbfadf4e
oleksandrtkv <oleksandr.tkv@gmail.com>
2023-10-10 10:24:36 +0300

    complete

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
 public class Main {
     private double EPS = 1e-6;
     private final double a = -0.5;
@@ -7,7 +10,7 @@ public class Main {
 
     public double f(double x) {
         if (x < 0.7 + EPS) return 1;
-        if (x > 1.4 + EPS) return Math.exp(a*x) * Math.cos(b*x);
+        if (x > 1.4 + EPS) return Math.exp(a * x) * Math.cos(b * x);
         return a * x * x * Math.log(x);
     }
 
@@ -16,26 +19,73 @@ public class Main {
     }
 
     public double[] fillX(double start, double end, double step) {
-        double[] x = new double[steps(start, end, step)];
-        for (int i = 0; i < x.length; i++){
-            x[i] = start + i * step;
-        }
-        return x;
+        return IntStream.range(0, steps(start, end, step)).mapToDouble(i -> start + i * step).toArray();
     }
 
     public double[] fillY(double[] x) {
-        double[] y = new double[x.length];
-        for (int i = 0; i < x.length; i++){
-            y[i] = f(x[i]);
-        }
-        return y;
+        return DoubleStream.of(x).map(this::f).toArray();
     }
 
     public double sum(double[] arr) {
-        double res = 0;
-        for (double v : arr) {
-            res += v;
+        return DoubleStream.of(arr).sum();
+    }
+
+    public double avg(double[] arr) {
+        return DoubleStream.of(arr).average().orElse(0);
+    }
+
+    public int findIndexOfMin(double[] arr) {
+        int indexOfMin = 0;
+        double min = arr[indexOfMin];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                indexOfMin = i;
+                min = arr[indexOfMin];
+            }
         }
-        return res;
+        return indexOfMin;
+    }
+
+    public int findIndexOfMax(double[] arr) {
+        int indexOfMax = 0;
+        double max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                indexOfMax = i;
+                max = arr[indexOfMax];
+            }
+        }
+        return indexOfMax;
+    }
+
+    public void printMax(double[] x, double[] y){
+        int indexOfMax = findIndexOfMax(y);
+        double yMax = y[indexOfMax];
+        double xMax = x[indexOfMax];
+
+        System.out.printf("Ymax = %6.3f with x = %6.3f and index is %d\n", yMax, xMax, indexOfMax);
+    }
+
+    public void printMin(double[] x, double[] y){
+        int indexOfMin = findIndexOfMin(y);
+        double yMin = y[indexOfMin];
+        double xMin = x[indexOfMin];
+
+        System.out.printf("Ymin = %6.3f with x = %6.3f and index is %d\n", yMin, xMin, indexOfMin);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    private void run() {
+        double[] x = fillX(0.0, 3.0, 0.004);
+        double[] y = fillY(x);
+        double sum = sum(y);
+        double avg = avg(y);
+        printMax(x, y);
+        printMin(x, y);
+        System.out.printf("sum = %6.3f\navg = %6.3f\n", sum, avg);
     }
 }
--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -1,9 +1,10 @@
 package main;
 
+import org.assertj.core.data.Percentage;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.*;
 
 public class MainTest {
@@ -20,13 +21,13 @@ public class MainTest {
     @Test(dataProvider = "dataForTest", dataProviderClass = StaticDataProvider.class)
     public void testF(double x, double expected) {
         double actual = main.f(x);
-        assertEquals(actual, expected, EPS);
+        assertThat(actual).isCloseTo(expected, Percentage.withPercentage(0.1));
     }
 
     @Test(dataProvider = "dataForSteps", dataProviderClass = StaticDataProvider.class)
     public void testSteps(double start, double end, double step, int expected) {
         int actual = main.steps(start, end, step);
-        assertEquals(actual, expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test(dataProviderClass = StaticDataProvider.class, dataProvider = "dataForFillX")
@@ -36,7 +37,7 @@ public class MainTest {
         double step = 0.004;
 
         double[] actual = main.fillX(start, end, step);
-        assertEquals(actual[index], expected, EPS);
+        assertThat(actual[index]).isCloseTo(expected, Percentage.withPercentage(0.1));
     }
 
     @Test(dataProviderClass = StaticDataProvider.class, dataProvider = "dataForFillY")
@@ -54,5 +55,29 @@ public class MainTest {
         double expected = 7.1;
         double actual = main.sum(arr);
         assertEquals(actual, expected, EPS);
+    }
+
+    @Test
+    public void testAvg() {
+        double[] arr = {1.1, 2.5, -7.0, 10.0, 0.5};
+        double expected = 1.42;
+        double actual = main.avg(arr);
+        assertEquals(actual, expected, EPS);
+    }
+
+    @Test
+    public void testFindIndexOfMin() {
+        double[] arr = {1.1, 2.5, -7.0, 10.0, 0.5};
+        int expected = 2;
+        int actual = main.findIndexOfMin(arr);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testFindIndexOfMax() {
+        double[] arr = {1.1, 2.5, -7.0, 10.0, 0.5};
+        int expected = 3;
+        int actual = main.findIndexOfMax(arr);
+        assertEquals(actual, expected);
     }
 }
